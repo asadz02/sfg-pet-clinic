b0c670eb07ed3accb492cd18fd228831517fe82e
asadz02 <eesedzade@gmail.com>
2025-05-29 16:10:21 +0400

    adding base entity. closes #11

--- pet-clinic-data/src/main/java/asad/springframework/model/Person.java
+++ pet-clinic-data/src/main/java/asad/springframework/model/Person.java
@@ -1,6 +1,6 @@
 package asad.springframework.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/asad/springframework/model/PetType.java
+++ pet-clinic-data/src/main/java/asad/springframework/model/PetType.java
@@ -1,6 +1,6 @@
 package asad.springframework.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
